f9ad730f53960c54e7ffbba5982bc48e6d47cf6b
John Carlson <jawnnypoo@gmail.com>
2016-04-12 15:11:32 -0500

    :earth_americas:

---
+++ README.md
@@ -0,0 +1,53 @@
+# Gimbal
+Assists with orientation locking and sensor event normalization
+
+There are many apps that lock orientation. There are good and bad reasons for this. But many times, locking a single activity within your app would suffice. Gimbal serves that purpose. When you navigate to an activity, it will lock the device to the orientation that the user was in when they entered. This way, if the user prefers to use your app in landscape, they will not have to be forced into portrait just because your app is not able to handle orientation on a screen for whatever reason
+
+[![Build Status](https://travis-ci.org/Commit451/Gimbal.svg?branch=master)](https://travis-ci.org/Commit451/Gimbal)
+
+# Gradle Dependency
+
+Add this in your root `build.gradle` file (**not** your module `build.gradle` file):
+
+```gradle
+allprojects {
+	repositories {
+		...
+		maven { url "https://jitpack.io" }
+	}
+}
+```
+
+Then, add the library to your project `build.gradle`
+```gradle
+dependencies {
+    compile 'com.github.Commit451:Gimbal:0.0.1'
+}
+```
+
+# Usage
+If you are using this library to lock orientation on certain activities, usage is simple:
+```java
+//this == activity
+Gimbal gimbal = new Gimbal(this);
+gimbal.lock();
+//later, if need be
+gimbal.unlock();
+```
+
+License
+--------
+
+    Copyright 2016 Commit 451
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ gimbal/src/main/java/com/commit451/gitbal/Gimbal.java
@@ -0,0 +1,167 @@
+package com.commit451.gitbal;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
+import android.hardware.SensorEvent;
+import android.view.Display;
+import android.view.Surface;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Tool to help out with rotation based things
+ */
+public class Gimbal {
+
+    private WeakReference<Activity> mActivityWeakReference;
+    private Point mNormalizationPoint;
+    private Point mDisplayPoint;
+
+    public Gimbal(Activity activity) {
+        mActivityWeakReference = new WeakReference<>(activity);
+    }
+
+    /**
+     * Locks the activity to the orientation the user was in when they entered the activity.
+     * @return true if lock was successful, false if it was not
+     */
+    public boolean lock() {
+        Activity activity = mActivityWeakReference.get();
+        if (activity == null) {
+            return false;
+        }
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        int rotation = display.getRotation();
+        int height;
+        int width;
+        if (mDisplayPoint == null) {
+            mDisplayPoint = new Point();
+        }
+        display.getSize(mDisplayPoint);
+        height = mDisplayPoint.y;
+        width = mDisplayPoint.x;
+        switch (rotation) {
+            case Surface.ROTATION_90:
+                if (width > height) {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+                else {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                }
+                break;
+            case Surface.ROTATION_180:
+                if (height > width) {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                }
+                else {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                }
+                break;
+            case Surface.ROTATION_270:
+                if (width > height) {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                }
+                else {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+                break;
+            case Surface.ROTATION_0:
+            default :
+                if (height > width) {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+                else {
+                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+        }
+        return true;
+    }
+
+    /**
+     * Unlock the previously locked activity
+     */
+    public boolean unlock() {
+        Activity activity = mActivityWeakReference.get();
+        if (activity == null) {
+            return false;
+        }
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        return true;
+    }
+
+    /**
+     * Normalize a {@link SensorEvent} from the gravity sensor for the current orientation. Has chance to fail at normalization if the activity no longer exists or has a window
+     * @param event the {@link SensorEvent} which will be normalized, typically acquired in a {@link android.hardware.SensorEventListener} event
+     */
+    public void normalizeGravityEvent(SensorEvent event) {
+        Activity activity = mActivityWeakReference.get();
+        if (activity == null) {
+            return;
+        }
+        Display display = activity.getWindow().getWindowManager().getDefaultDisplay();
+        int rotation = display.getRotation();
+        int height;
+        int width;
+        if (mNormalizationPoint == null) {
+            mNormalizationPoint = new Point();
+        }
+        display.getSize(mNormalizationPoint);
+        height = mNormalizationPoint.y;
+        width = mNormalizationPoint.x;
+        float x;
+        float y;
+        switch (rotation) {
+            case Surface.ROTATION_90:
+                if (width > height) {
+                    //landscape
+                    x = -event.values[1];
+                    y = event.values[0];
+                }
+                else {
+                    //reverse portrait
+                    x = -event.values[0];
+                    y = -event.values[1];
+                }
+                break;
+            case Surface.ROTATION_180:
+                if (height > width) {
+                    //reverse portrait
+                    x = -event.values[0];
+                    y = -event.values[1];
+                }
+                else {
+                    //reverse landscape
+                    x = event.values[1];
+                    y = -event.values[0];
+                }
+                break;
+            case Surface.ROTATION_270:
+                if (width > height) {
+                    //reverse landscape
+                    x = event.values[1];
+                    y = -event.values[0];
+                }
+                else {
+                    //portrait
+                    x = event.values[0];
+                    y = event.values[1];
+                }
+                break;
+            case Surface.ROTATION_0:
+            default :
+                if (height > width) {
+                    //portrait
+                    x = event.values[0];
+                    y = event.values[1];
+                }
+                else {
+                    //landscape
+                    x = -event.values[1];
+                    y = event.values[0];
+                }
+        }
+        event.values[0] = x;
+        event.values[1] = y;
+    }
+}
